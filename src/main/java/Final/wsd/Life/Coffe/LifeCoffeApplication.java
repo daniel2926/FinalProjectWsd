288c14fa2cefdfc5a2b7db38b0b307a4d52c4e62
Daniel <danielsimamorasimamora@gmail.com>
2023-10-25 16:19:21 +0700

    File baru

---
+++ src/main/java/Final/wsd/Life/Coffe/LifeCoffeApplication.java
@@ -0,0 +1,13 @@
+package Final.wsd.Life.Coffe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeCoffeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeCoffeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Final/wsd/Life/Coffe/LifeCoffeApplicationTests.java
@@ -0,0 +1,13 @@
+package Final.wsd.Life.Coffe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeCoffeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
