d952984c939efc51632efbfde2af677bd802928f
Daniel <danielsimamorasimamora@gmail.com>
2023-10-29 21:35:07 +0700

    baru menambahkan MainMember

---
+++ src/main/java/Final/wsd/Life/Coffe/member/MainMember.java
@@ -0,0 +1,8 @@
+package Final.wsd.Life.Coffe.member;
+
+public class MainMember {
+    public static void main(String[] args) {
+        System.out.println("ini punya daniel yosh apriando ");
+        System.out.println(26 + 4);
+    }
+}
