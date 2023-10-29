28446e7ae92029ab1e09e9416116234866f24e6c
Daniel <danielsimamorasimamora@gmail.com>
2023-10-30 00:52:05 +0700

    menambahkan rateDiscount

---
+++ src/main/java/Final/wsd/Life/Coffe/discount/RateDiscountPolicy.java
@@ -0,0 +1,25 @@
+package Final.wsd.Life.Coffe.discount;
+
+import Final.wsd.Life.Coffe.member.Grade;
+import Final.wsd.Life.Coffe.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    int discountPercentSilver = 10;
+    int discountPercentGold = 20;
+    public int discountSilver(Member member, int price) {
+        if(member.getGrade() == Grade.SILVER){
+            return price * discountPercentSilver /100;
+        }
+        else{
+            return 0;
+        }
+    }
+    public int discountGold(Member member, int price) {
+        if(member.getGrade() == Grade.SILVER){
+            return price * discountPercentGold/100;
+        }
+        else{
+            return 0;
+        }
+    }
+}
