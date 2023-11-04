8c55a8588dfa378570c95b2daf750461b44375e1
PrimeHegel <hegelfernando5@gmail.com>
2023-11-04 19:29:32 +0700

    Memperbarui fitur XYZ

--- src/main/java/Final/wsd/Life/Coffe/SpringConfig.java
+++ src/main/java/Final/wsd/Life/Coffe/SpringConfig.java
@@ -1,5 +1,24 @@
 package Final.wsd.Life.Coffe;
 
+
+import Final.wsd.Life.Coffe.member.MemberRepository;
+import Final.wsd.Life.Coffe.member.MemoryMemberRepository;
+import Final.wsd.Life.Coffe.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SpringConfig {
+    @Bean
+    public MemberService memberService(){
+
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+
+        return new MemoryMemberRepository();
+    }
 
 }
---
+++ src/main/java/Final/wsd/Life/Coffe/service/MemberService.java
@@ -0,0 +1,41 @@
+package Final.wsd.Life.Coffe.service;
+
+import Final.wsd.Life.Coffe.member.Member;
+import Final.wsd.Life.Coffe.member.MemberRepository;
+import Final.wsd.Life.Coffe.member.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService{
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+            this.memberRepository = memberRepository;
+        }
+        //SignIn Member
+
+        public long join(Member member){
+            validDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        }
+
+        private void validDuplicateMember(Member member) {
+            Optional<Member> result = memberRepository.findByName(member.getName());
+            result.ifPresent(m -> {
+                throw new IllegalStateException("You are already a member.");
+            });
+        }
+
+        public List<Member> findMember(){
+            return memberRepository.findAll();
+        }
+
+        public Optional<Member> findOne(Long memberId){
+            return memberRepository.findById(memberId);
+        }
+    }
+
+}
