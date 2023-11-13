346edd892b4c9005017c86ba254ef7ad7e8d7f80
Daniel <danielsimamorasimamora@gmail.com>
2023-11-13 11:56:08 +0700

    controller dari bapak nya

--- src/main/java/Final/wsd/Life/Coffe/Controller/HomeController.java
+++ src/main/java/Final/wsd/Life/Coffe/Controller/HomeController.java
@@ -1,4 +1,13 @@
-package Final.wsd.Life.Coffe.Controller;
+package Final.wsd.Life.Coffe.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
+
